b04320dfa04da554ad9486f686c9652244434d3d
L-y-u-d-m-y-l-a <lyudmyla.k@hotmail.com>
2024-04-07 10:10:45 +0100

    result_js_hw_03_upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
-    let filteredArray = [];
-
+    const filteredArray = [];
+    
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             filteredArray.push(numbers[i]);
